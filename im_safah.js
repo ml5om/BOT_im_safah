2615475c70f1d993352cff29354ef573c7eb6a08
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-29 12:53:44 +0300

    Update im_safah.js

--- im_safah.js
+++ im_safah.js
@@ -1,4 +1,11 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
 const client = new Discord.Client();
 const prefix = '+'
 
